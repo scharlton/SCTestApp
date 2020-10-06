d05cb920390fc1c7513cac9eef6a6ddb7694fa1d
Stefon Charlton <stefon.charlton@gmail.com>
2020-10-06 16:15:22 +0100

    Added code for testing distribution groups

--- SCTestApp/SCTestApp/App.xaml.cs
+++ SCTestApp/SCTestApp/App.xaml.cs
@@ -6,6 +6,9 @@ using SCTestApp.Views;
 using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Distribute;
+using System.Threading.Tasks;
 
 namespace SCTestApp
 {
@@ -22,10 +25,53 @@ namespace SCTestApp
 
         protected override void OnStart()
         {
+            Distribute.DisableAutomaticCheckForUpdate();
+            Distribute.ReleaseAvailable = OnReleaseAvailable;
             AppCenter.Start("android=92762260-34ff-4643-8126-ef36006ea1d8;" +
                   "uwp={5ab647aaa28c254291bb51e5134c3e39577d48c7};" +
-                  "ios={c1b5f5338cce61a4921f94e94c9531e6c4d23f1f}",
+                  "ios={c1b5f5338cce61a4921f94e94c9531e6c4d23f1f}", typeof(Distribute),
                   typeof(Analytics), typeof(Crashes));
+        }
+
+        private bool OnReleaseAvailable(ReleaseDetails releaseDetails)
+        {
+            // Look at releaseDetails public properties to get version information, release notes text or release notes URL
+            string versionName = releaseDetails.ShortVersion;
+            string versionCodeOrBuildNumber = releaseDetails.Version;
+            string releaseNotes = releaseDetails.ReleaseNotes;
+            Uri releaseNotesUrl = releaseDetails.ReleaseNotesUrl;
+
+            // custom dialog
+            var title = "Version " + versionName + " available!";
+            Task answer;
+
+            // On mandatory update, user cannot postpone
+            if (releaseDetails.MandatoryUpdate)
+            {
+                answer = Current.MainPage.DisplayAlert(title, releaseNotes, "Download and Install");
+            }
+            else
+            {
+                answer = Current.MainPage.DisplayAlert(title, releaseNotes, "Download and Install", "Maybe tomorrow...");
+            }
+            answer.ContinueWith((task) =>
+            {
+                // If mandatory or if answer was positive
+                if (releaseDetails.MandatoryUpdate || (task as Task<bool>).Result)
+                {
+                    // Notify SDK that user selected update
+                    Distribute.NotifyUpdateAction(UpdateAction.Update);
+                }
+                else
+                {
+                    // Notify SDK that user selected postpone (for 1 day)
+                    // Note that this method call is ignored by the SDK if the update is mandatory
+                    Distribute.NotifyUpdateAction(UpdateAction.Postpone);
+                }
+            });
+
+            // Return true if you are using your own dialog, false otherwise
+            return true;
         }
 
         protected override void OnSleep()
